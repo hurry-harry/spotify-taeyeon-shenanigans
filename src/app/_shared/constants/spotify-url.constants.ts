84a0e7269eb95e8ffa5200d68c0604d9cf0e73d4
hurry-harry <harold.francisco1412@gmail.com>
2024-01-11 17:23:04 +0800

    Leftover commit

---
+++ src/app/_shared/constants/spotify-url.constants.ts
@@ -0,0 +1,6 @@
+export const SPOTIFY_MY_PROFILE: string = "https://api.spotify.com/v1/me";
+export const SPOTIFY_MY_TOP_ITEMS: string = "https://api.spotify.com/v1/me/top";
+export const SPOTIFY_ALBUMS: string = "https://api.spotify.com/v1/albums";
+export const SPOTIFY_NEW_RELEASES: string = "browse/new-releases";
+
+
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+import { Environment } from "../app/_shared/models/environment.model";
+
+export const environment: Environment = {
+    apiUrl: "http://localhost:8888/" // replace with whatever host provider for the server-side
+}

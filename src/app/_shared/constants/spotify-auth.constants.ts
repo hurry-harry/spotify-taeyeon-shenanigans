49f4f01c0f63781fd5942b4d98e0411f59791955
hurry-harry <harold.francisco1412@gmail.com>
2024-03-01 14:23:10 +0800

    Re-change redirect URL to the Vercel one

--- src/app/_shared/constants/spotify-auth.constants.ts
+++ src/app/_shared/constants/spotify-auth.constants.ts
@@ -1,3 +1,3 @@
 export const SPOTIFY_CLIENT_ID: string = "964ff9c556874c2fb2fa083aec3bc177";
-export const SPOTIFY_REDIRECT_URI: string = "http://localhost:4200/callback/"; // local
-// export const SPOTIFY_REDIRECT_URI: string = "https://taeyeon-heardle.vercel.app/callback/"; // vercel
+// export const SPOTIFY_REDIRECT_URI: string = "http://localhost:4200/callback/"; // local
+export const SPOTIFY_REDIRECT_URI: string = "https://taeyeon-heardle.vercel.app/callback/"; // vercel
